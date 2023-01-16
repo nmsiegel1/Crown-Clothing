86a82163a740b8b38f5fa172b800385dbefac4cc
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2023-01-16 15:17:55 -0500

    add routing for category pages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
-import { ProductsProvider } from "./contexts/products.context";
+import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
 import "./index.scss";
@@ -17,9 +17,9 @@ root.render(
     <BrowserRouter>
       <CartProvider>
         <UserProvider>
-          <ProductsProvider>
+          <CategoriesProvider>
             <App />
-          </ProductsProvider>
+          </CategoriesProvider>
         </UserProvider>
       </CartProvider>
     </BrowserRouter>
