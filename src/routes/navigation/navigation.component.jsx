56a06c91c02a6324a59d82b8a8af1492375d7b7c
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2023-01-25 20:27:10 -0500

    migrate cart context to redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,21 +1,18 @@
-import { useContext } from "react"
 import { Outlet } from "react-router-dom"
 import { useSelector } from "react-redux"
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import { CartContext } from "../../contexts/cart.context"
 import { signOutUser } from "../../utils/firebase/firebase.utils"
 import { LogoContainer, NavigationContainer, NavLink, NavLinks,  } from "./navigation.styles"
 import { selectCurrentUser } from "../../store/user/user.selector"
-
+import { selectIsCartOpen } from "../../store/cart/cart.selector"
 
 import CartIcon from "../../components/cart-icon/cart-icon.component"
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
 
 const NavBar = () => {
 	const currentUser = useSelector(selectCurrentUser)
-	const { isCartOpen } = useContext(CartContext)
-
+	const isCartOpen = useSelector(selectIsCartOpen)
 
 	return (
 		<>
